7d50f06ca6fde163770f736bd389d2de5a679eb9
dilshankunnummal <dilshankunnummmal@gmail.com>
2025-07-31 20:57:45 +0530

    splash screen created

---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/images/bg.png', fit: BoxFit.cover),
+          Center(
+            child: Image.asset(
+              'assets/images/logo.png',
+              width: 150,
+              height: 150,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
