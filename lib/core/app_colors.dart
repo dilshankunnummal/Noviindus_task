4a4193c16c6069196509279fe8465574b70173cf
dilshankunnummal <dilshankunnummmal@gmail.com>
2025-07-31 20:42:11 +0530

    created app_route and app_colors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF006837);
+  static const Color textFieldBg = Color(0x40D9D9D9);
+  static const Color borderGrey = Colors.grey;
+  static const Color textBlack = Colors.black;
+  static const Color textGrey = Colors.grey;
+  static const Color white = Colors.white;
+}
